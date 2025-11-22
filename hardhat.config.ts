285445cd60573487ed39344fa375ed340d7e3917
hiroshi <hr_deep@me.com>
2025-11-22 19:41:36 +0900

    feat: Verify contract on Amoy and confirm manual minting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    amoy: {
+      url: process.env.POLYGON_AMOY_RPC_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    // 最新仕様に合わせて修正
+    apiKey: process.env.POLYGONSCAN_API_KEY || "",
+    customChains: [
+      {
+        network: "amoy",
+        chainId: 80002,
+        urls: {
+          apiURL: "https://api-amoy.polygonscan.com/api",
+          browserURL: "https://amoy.polygonscan.com"
+        }
+      }
+    ]
+  },
 };
 
 export default config;
---
+++ scripts/arguments.js
@@ -0,0 +1,3 @@
+module.exports = [
+  "0x024f97a8b6009EAcBe938CB465d078b5ADeD0cFA" // あなたの開発用アドレス
+];
